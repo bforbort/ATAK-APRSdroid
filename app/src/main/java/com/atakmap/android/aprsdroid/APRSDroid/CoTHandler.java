ab8e9bb07500ec0fc3bdef70412031dbab3b72ff
bforbort <github@k1qv.com>
2021-04-05 01:33:55 -0700

    Update CoTHandler.java

--- app/src/main/java/com/atakmap/android/aprsdroid/APRSDroid/CoTHandler.java
+++ app/src/main/java/com/atakmap/android/aprsdroid/APRSDroid/CoTHandler.java
@@ -3,7 +3,6 @@ package com.atakmap.android.aprsdroid.APRSDroid;
 import com.atakmap.android.maps.Marker;
 import com.atakmap.android.user.PlacePointTool;
 import com.atakmap.coremap.maps.coords.GeoPoint;
-import com.atakmap.coremap.maps.coords.GeoPointMetaData;
 
 import static com.atakmap.android.maps.MapView.getMapView;
 
@@ -12,22 +11,20 @@ public class CoTHandler {
                                       int course, int speed, String comment,
                                       char symbolTable, char symbolCode) {
         PlacePointTool.MarkerCreator mc = new PlacePointTool.MarkerCreator(geoPoint);
+
         mc.setUid("APRS." + callsign);
         mc.setCallsign(callsign);
         mc.setType("a-f-G");
+
         mc.showCotDetails(false);
         mc.setNeverPersist(true);
+
         Marker m = mc.placePoint();
-        // the stle of the marker is by default set to show an arrow, this will allow for full
-        // rotation.   You need to enable the heading mask as well as the noarrow mask
+
         m.setPoint(geoPoint);
-//        m.setStyle(m.getStyle()
-//                | Marker.STYLE_ROTATE_HEADING_MASK
-//                | Marker.STYLE_ROTATE_HEADING_NOARROW_MASK);
         m.setTrack(course, speed);
-//        m.setMetaInteger("color", Color.YELLOW);
-//       m.setMetaString(IconsetPath,
-//                "34ae1613-9645-4222-a9d2-e5f243dea2865/Military/A10.png");
+        m.setRemarks(comment);
+
         m.refresh(getMapView().getMapEventDispatcher(), null,
                 CoTHandler.class);
     }
