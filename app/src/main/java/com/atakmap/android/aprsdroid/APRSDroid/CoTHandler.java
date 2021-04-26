93b7624af89561c5177e248d7a91415a9b220b37
bforbort <github@k1qv.com>
2021-04-26 00:08:22 -0700

    Update CoTHandler.java
    Add a 30 minute stale
    Broadcast RX'ed APRS messages over network as CoT
    Fixed speed

--- app/src/main/java/com/atakmap/android/aprsdroid/APRSDroid/CoTHandler.java
+++ app/src/main/java/com/atakmap/android/aprsdroid/APRSDroid/CoTHandler.java
@@ -1,7 +1,10 @@
 package com.atakmap.android.aprsdroid.APRSDroid;
 
+import com.atakmap.android.cot.CotMapComponent;
+import com.atakmap.android.importexport.CotEventFactory;
 import com.atakmap.android.maps.Marker;
 import com.atakmap.android.user.PlacePointTool;
+import com.atakmap.coremap.cot.event.CotEvent;
 import com.atakmap.coremap.maps.coords.GeoPoint;
 
 import static com.atakmap.android.maps.MapView.getMapView;
@@ -17,15 +20,21 @@ public class CoTHandler {
         mc.setType("a-f-G");
 
         mc.showCotDetails(false);
-        mc.setNeverPersist(true);
+        mc.setNeverPersist(false);
 
         Marker m = mc.placePoint();
 
         m.setPoint(geoPoint);
-        m.setTrack(course, speed);
+        m.setTrack(course, (double) speed);
         m.setRemarks(comment);
 
-        m.refresh(getMapView().getMapEventDispatcher(), null,
-                CoTHandler.class);
+        // TODO: make this configurable
+        // 30 minute TTL
+        m.setMetaLong("autoStaleDuration", 1800000L);
+        m.setMetaDouble("Speed", (double) speed);
+
+        final CotEvent cotEvent = CotEventFactory.createCotEvent(m);
+        // TODO: add Internal only option
+        CotMapComponent.getExternalDispatcher().dispatchToBroadcast(cotEvent);
     }
 }
